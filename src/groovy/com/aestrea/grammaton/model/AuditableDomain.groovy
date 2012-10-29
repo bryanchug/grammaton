8478018cf9e28d4a4a90cd98695837bbe061dcaf
Gerald Chua <gerald.chua@aestrea.com>
2012-10-29 20:33:53 +0800

    added createdBy and updatedBy to AuditableDomain

--- src/groovy/com/aestrea/grammaton/model/AuditableDomain.groovy
+++ src/groovy/com/aestrea/grammaton/model/AuditableDomain.groovy
@@ -1,8 +1,36 @@
 package com.aestrea.grammaton.model
 
+import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
+import grails.plugins.springsecurity.SpringSecurityService
+import org.codehaus.groovy.grails.commons.ApplicationHolder
+
 abstract class AuditableDomain {
 
     Date lastUpdated
     Date dateCreated
+    String createdBy
+    String updatedBy
+
+    static constraints = {
+        createdBy nullable: true
+        updatedBy nullable: true
+    }
+
+    private static SpringSecurityService getSpringSecurityServiceBean() {
+        return ApplicationHolder.application.getMainContext().getBean('springSecurityService')
+    }
+
+    private static String getCurrentUsername() {
+        getSpringSecurityServiceBean().getPrincipal()?.username
+    }
+
+    def beforeUpdate () {
+        updatedBy = getCurrentUsername() ?: updatedBy
+    }
+
+    def beforeInsert (){
+        createdBy = getCurrentUsername() ?: createdBy
+        updatedBy = createdBy
+    }
 
 }
